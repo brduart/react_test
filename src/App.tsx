c9165d08eee4715c7391651887289a6b5bc0f1ec
Bruno Duarte <bruno.duart98@gmail.com>
2024-07-08 11:16:13 -0300

    criação do react hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
+import { SubmitHandler, useForm } from "react-hook-form";
 import "./App.css";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Posts from "./components/Posts";
-import { PostProvider } from "./contexts/PostsContext";
+
+//TIPAGEM INPUTS
+type Inputs = {
+  name: string;
+  lastName: string;
+};
 
 function App() {
+  //CHAMADA DO HOOK FORM
+  const { handleSubmit, register } = useForm<Inputs>();
+
+  //FUNÇÃO SUBMIT
+  const handleFormSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
+  //MONTAGEM DO FORM
   return (
     <>
-      <PostProvider>
-        <Header />
-        <Footer />
-        <Posts />
-      </PostProvider>
+      <div className="container mx-auto">
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
+          <input
+            {...register("name")}
+            placeholder="Digite seu nome"
+            className="border border-white p-3 text-black"
+          />
+
+          <input
+            {...register("lastName")}
+            placeholder="Digite seu sobrenome"
+            className="block mt-4 border border-white p-3 text-black"
+          />
+
+          <input type="submit" value="Enviar" className="text-white" />
+        </form>
+      </div>
     </>
   );
 }
