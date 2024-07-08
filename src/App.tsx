4d02f77be8dabbb0587af867bfeb7ca49a8b426a
Bruno Duarte <bruno.duart98@gmail.com>
2024-07-08 16:28:37 -0300

    Definindo mensagem de erro no Zod

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,22 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 const SignUpFormSchema = z.object({
-  name: z.string().min(2).max(20),
+  name: z
+    .string()
+    .min(2, { message: "Precisa ter pelo menos 2 letras" })
+    .max(20, { message: "máximo de 20 letras" }),
   lastName: z.string().optional(),
-  age: z.number().min(18),
+  age: z
+    .number({ invalid_type_error: "idade precisa ser um numero" })
+    .min(18, { message: "minimo de 18 anos" }),
 });
 
 function App() {
-  const { register, handleSubmit } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     resolver: zodResolver(SignUpFormSchema),
   });
 
@@ -24,18 +33,27 @@ function App() {
     <>
       <div className="container mx-auto">
         <form onSubmit={handleSubmit(handleSignUpForm)}>
-          <input
-            {...register("name")}
-            className="border border-white p-3 m-3 text-black"
-          />
-          <input
-            {...register("lastName")}
-            className="border border-white p-3 m-3 text-black"
-          />
-          <input
-            {...register("age", { valueAsNumber: true })}
-            className="border border-white p-3 m-3 text-black"
-          />
+          <div>
+            <input
+              {...register("name")}
+              className="border border-white p-3 m-3 text-black"
+            />
+            {errors.name && <p>{errors.name.message as string}</p>}
+          </div>
+          <div>
+            <input
+              {...register("lastName")}
+              className="border border-white p-3 m-3 text-black"
+            />
+            {errors.lastName && <p>{errors.lastName.message as string}</p>}
+          </div>
+          <div>
+            <input
+              {...register("age", { valueAsNumber: true })}
+              className="border border-white p-3 m-3 text-black"
+            />
+            {errors.age && <p>{errors.age.message as string}</p>}
+          </div>
 
           <input type="submit" value="Cadastrar" className="text-white" />
         </form>
