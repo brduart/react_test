a81145672199477b9cbd82148ff7724c241c6e9e
Bruno Duarte <bruno.duart98@gmail.com>
2024-07-08 15:20:28 -0300

    Integrando com bibliotecas UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { SubmitHandler, useForm } from "react-hook-form";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import "./App.css";
-import { SignUpForm } from "./types/SignupForm";
-import Input from "./components/Input";
+import { SignUpForm } from "./types/SignUpForm";
+import { Input } from "@mui/material";
 
 function App() {
   //CHAMADA DO HOOK FORM
-  const { control, handleSubmit, setValue } = useForm<SignUpForm>();
+  const { control, handleSubmit } = useForm<SignUpForm>();
 
   //FUNÇÃO SUBMIT
   const handleFormSubmit: SubmitHandler<SignUpForm> = (data) => {
@@ -17,32 +17,34 @@ function App() {
     <>
       <div className="container mx-auto">
         <form onSubmit={handleSubmit(handleFormSubmit)}>
-          <Input
+          <Controller
             control={control}
             name="name"
-            rules={{
-              required: true,
-              minLength: 2,
-              maxLength: 20,
-            }}
+            render={({ field, fieldState }) => (
+              <Input
+                {...field}
+                error={fieldState.invalid}
+                style={{ background: "#fff" }}
+              />
+            )}
+            rules={{ required: true, minLength: 2, maxLength: 20 }}
           />
 
-          <Input control={control} name="lastName" />
+          <Controller
+            control={control}
+            name="lastName"
+            render={({ field }) => <Input {...field} />}
+          />
 
-          <Input
+          <Controller
             control={control}
             name="age"
-            rules={{
-              required: "campo obrigatorio",
-              min: 18,
-              max: 120,
-            }}
+            render={({ field }) => <Input {...field} />}
+            rules={{ required: true, min: 18 }}
           />
 
           <input type="submit" value="Enviar" className="text-white" />
         </form>
-
-        <button onClick={() => setValue("age", 18)}>Definir idade</button>
       </div>
     </>
   );
