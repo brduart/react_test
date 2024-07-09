a84c32bc8e0026bc12f9645290d85fd5301e9e0c
Bruno Duarte <bruno.duart98@gmail.com>
2024-07-09 10:56:26 -0300

    react router dom : searchparams(query string)

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,8 +3,18 @@ import { Link, useSearchParams } from "react-router-dom";
 const About = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
+  const setOrder = (order: "asc" | "desc") => {
+    searchParams.set("order", order);
+    setSearchParams(searchParams);
+  };
+
   return (
     <div>
+      Filter: {searchParams.get("filter")}
+      Order: {searchParams.get("order")}
+      <hr />
+      <button onClick={() => setOrder("asc")}>ASC</button>~
+      <button onClick={() => setOrder("desc")}>DESC</button>
       <div>
         <Link to="/about/about1">ABOUT 1</Link>
         <Link to="/about/about2">ABOUT 2</Link>
